854e2213849ec79517b8ce4510c94b325c25de02
gabby-delforge <gabrielle.delforge@gmail.com>
2017-10-06 13:48:37 -0700

    Initial commmit

---
+++ twittertest.py
@@ -0,0 +1,43 @@
+import tweepy
+import _json
+from tweepy.auth import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+consumer_key = "FTdfjeihzxaUVKxGZvILikLs0"
+consumer_secret = "c7D1pHZd77q2bV0L61M729WyM58d34xi3wF1cKOvUemnhNQ2pl"
+
+access_token =	"347981696-6scrrVQWcTDaVZLlq6uTpBaRa4RiVSe6UNl4IJeD"
+access_secret = "eW89ZrfLCKweiLuXmTNbljjaCRMcUxkYUEcWn756Ztpuj"
+
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+def process_tweet(tweet):
+    print(tweet)
+
+#Prints 10 homepage tweets
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    process_tweet(status.text)
+
+
+class MyListener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
