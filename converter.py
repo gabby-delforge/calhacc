e20014b6abf3dd0354d5628bf7ccf98ca380e5ab
gabby-delforge <gabrielle.delforge@gmail.com>
2017-10-07 00:32:12 -0700

    Started skeleton for neural net

--- converter.py
+++ converter.py
@@ -14,8 +14,12 @@ def convert(imgs, labels):
 	with open(labels, 'rb') as open_label:
 		magic, num_img = struct.unpack(">II", open_label.read(8))
 			
+	print("Before image splicing")		
 	imgs_list = splice_imgs(imgs)
+	print("Before labels splicing")		
 	labels_list = splice_labels(labels)
+	print("After label splicing")		
+
 
 	get_tuple = lambda i: (imgs_list[i], labels_list[i])
 
@@ -23,6 +27,7 @@ def convert(imgs, labels):
 	for i in range(num_imgs):
 		merged += get_tuple(i)
 		
+	print(merged)
 	return merged
 
 """returns a list of lists where each nested list contains all the pixel values
@@ -39,11 +44,11 @@ def splice_imgs(img_file):
 			while pixels_count < num_pixels:
 				_next = pixels_stream.read(1)
 				pixel = struct.unpack("B", _next)
-				print(pixel[0])
+				#print(pixel[0])
 				img_pixels.append(pixel[0])
 
 				pixels_count += 1
-			print("i've reached the end of image " + str(num_imgs_read) + "!")
+			#print("i've reached the end of image " + str(num_imgs_read) + "!")
 			num_imgs_read += 1
 			result.append(img_pixels)
 	return result
@@ -66,6 +71,7 @@ def splice_labels(labels_file):
 			result.append(label[0])
 	return result
 
+convert("MNISTfiles/test_images", "MNISTfiles/test_labels")
 
 
 
--- lettertest.py
+++ lettertest.py
@@ -1,4 +1,37 @@
 import converter
 import numpy
 
-training_data = converter.convert(img, labels)
+training_data = converter.convert("MNISTfiles/test_images", "MNISTfiles/test_labels")
+
+class Network(object):
+	""" Sizes is an array whose length equals the number of layers in the NN, 
+	and whose values represent the number of nodes in each layer. 
+	Biases and weights are initialized randomly to start with. """
+	def __init__(self, sizes):
+
+	""" Given an input a, feeds the input through each layer to get an output. """
+	def get_output(self, a):
+
+	"""Uses stochastic gradient descent to minimize loss. 
+		rate = learning rate
+		epochs = number of times to run through training
+		mini_batch_size = size of mini_batch to use to update weights - can be resource-heavy
+		test_data = used for tracking progress, resource-heavy. """
+	def learn(self, rate, epochs, mini_batch_size, test_data = None):
+
+	"""Uses the training data in MINI_BATCH to update the network's weights and biases. """
+	def update_mini_batch(self, mini_batch, rate):
+
+
+def sigmoid(z):
+	return 1.0/(1.0 + np.exp(-z))
+
+"""Derivative of the sigmoid function. """
+def sigmoid_prime(z):
+
+
+
+
+
+
+
