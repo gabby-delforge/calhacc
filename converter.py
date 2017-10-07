9ff960b48f2efd1c60ff13c75a4cadfb37c363a7
gabby-delforge <gabrielle.delforge@gmail.com>
2017-10-06 18:11:23 -0700

    Wrote pseudocode for converter

--- converter.py
+++ converter.py
@@ -1,3 +1,13 @@
 import struct 
 from matplotlib import plot
 
+def convert(imgs, labels):
+	#Remove header
+	
+	#Splice imgs
+	#Splice labels
+	#Merge into 
+
+#DESIRED OUTPUT:
+#List of tuples, each tuple containing a list of pixel values and a label
+#[([1, 2, 3, 4], 5), ([1, 2, 3, 4], 5), ([1, 2, 3, 4], 5)]
