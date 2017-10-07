06e8e0f2c59e86e8725ac1aa7b9ea437a1934eaa
gabby-delforge <gabrielle.delforge@gmail.com>
2017-10-06 18:39:48 -0700

    Updated pseudocode

--- converter.py
+++ converter.py
@@ -3,10 +3,23 @@ from matplotlib import plot
 
 def convert(imgs, labels):
 	#Remove header
-	
+	header = 
+	num_imgs = 
+	num_rows = 
+	num_cols = 
+
 	#Splice imgs
+	#Returns a list of lists, num_imgs long
+	imgs = 
+
 	#Splice labels
-	#Merge into 
+	#Returns a list of labels, num_imgs long
+	labels = 
+
+	#Merge into correct format
+	merged = 
+	
+	return merged 
 
 #DESIRED OUTPUT:
 #List of tuples, each tuple containing a list of pixel values and a label
